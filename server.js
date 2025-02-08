67b7c7eb96d4b81c4c79e05b947c3bf05be218c2
Gunjan Rathi <gunjan.20.12@gmail.com>
2025-02-08 17:04:46 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const Redis = require("ioredis");
+
+const app = express();
+const redis = new Redis();
+const bannedIPs = new Set(["192.168.1.1"]); // Example banned IPs
+
+app.use((req, res, next) => {
+  const ip = req.ip;
+  if (bannedIPs.has(ip)) {
+    return res.status(403).json({ message: "Access denied" });
+  }
+  redis.lpush("request_queue", JSON.stringify({ ip, url: req.url }));
+  next();
+});
+
+app.get("/", (req, res) => res.send("Middleware Running"));
+
+app.listen(4000, () => console.log("Middleware running on port 4000"));
